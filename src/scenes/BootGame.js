de51298c0d5e856132e8c7d3862e9e0db97ce9ae
m31271n <m31271n@users.noreply.github.com>
2019-04-04 16:22:32 +0800

    feat: add preloader

--- src/scenes/BootGame.js
+++ src/scenes/BootGame.js
@@ -1,5 +1,5 @@
 import Phaser from 'phaser'
-import WebFont from 'webfontloader'
+
 import _rect from 'assets/rect.png'
 import _logo from 'assets/logo.png'
 import _background from 'assets/sprites/background.png'
@@ -11,14 +11,12 @@ import _branchRight from 'assets/sprites/branch_right.png'
 import _man from 'assets/sprites/man.png'
 import _man_json from 'assets/sprites/man.json'
 import _btn_play from 'assets/sprites/btn-play.png'
-
 import _theme_ogg from 'assets/sounds/theme.ogg'
 import _theme_mp3 from 'assets/sounds/theme.mp3'
 import _cut_ogg from 'assets/sounds/cut.ogg'
 import _cut_mp3 from 'assets/sounds/cut.mp3'
 import _death_ogg from 'assets/sounds/death.ogg'
 import _death_mp3 from 'assets/sounds/death.mp3'
-
 import _font from 'assets/fonts/stacked-pixel.ttf'
 
 class BootGame extends Phaser.Scene {
@@ -26,7 +24,62 @@ class BootGame extends Phaser.Scene {
     super('BootGame')
   }
 
+  init() {
+    const { width, height } = this.game.config
+    this.gameWidth = width
+    this.gameHeight = height
+    this.gameCenterX = width / 2
+    this.gameCenterY = height / 2
+  }
+
   preload() {
+    this.load.webFont('Stacked Pixel', _font)
+  }
+
+  create() {
+    const progressBoxX = 200
+    const progressBoxY = this.gameCenterY
+    const progressBoxWidth = this.gameWidth - 2 * progressBoxX
+    const progressBoxHeight = 70
+
+    const progressBarX = progressBoxX + 10
+    const progressBarY = progressBoxY + 10
+    const progressBarWidth = progressBoxWidth - 20
+    const progressBarHeight = progressBoxHeight - 20
+
+    const percentTextX = this.gameCenterX
+    const percentTextY = this.gameCenterY + progressBoxHeight / 2
+
+    const assetTextX = this.gameCenterX
+    const assetTextY = this.gameCenterY + 100
+
+    const progressBar = this.add.graphics()
+
+    const progressBox = this.add.graphics()
+    progressBox.fillStyle(0x222222, 0.5)
+    progressBox.fillRect(
+      progressBoxX,
+      progressBoxY,
+      progressBoxWidth,
+      progressBoxHeight
+    )
+
+    const percentText = this.add
+      .text(percentTextX, percentTextY, '0%', {
+        fontFamily: 'Stacked Pixel',
+        fontSize: 40,
+        color: '#ffffff',
+      })
+      .setOrigin(0.5)
+
+    const assetText = this.add
+      .text(assetTextX, assetTextY, '', {
+        fontFamily: 'Stacked Pixel',
+        fontSize: 40,
+        color: '#ffffff',
+      })
+      .setOrigin(0.5, 0.5)
+
     this.load.image('rect', _rect)
     this.load.image('logo', _logo)
 
@@ -45,39 +98,37 @@ class BootGame extends Phaser.Scene {
     this.load.audio('cut', [_cut_ogg, _cut_mp3])
     this.load.audio('death', [_death_ogg, _death_mp3])
 
-    this.load.on('complete', () => {
+    this.load.on('progress', function(progress) {
+      progressBar.clear()
+      progressBar.fillStyle(0xffffff, 1)
+      progressBar.fillRect(
+        progressBarX,
+        progressBarY,
+        progressBarWidth * progress,
+        progressBarHeight
+      )
+
+      percentText.setText(Number.parseInt(progress * 100) + '%')
+    })
+
+    this.load.on('fileprogress', function(file) {
+      assetText.setText('Loading asset: ' + file.src)
+    })
+
+    this.load.once('complete', () => {
+      progressBar.destroy()
+      progressBox.destroy()
+      percentText.destroy()
+      assetText.destroy()
+
       this.nextScene()
     })
-  }
 
-  create() {
-    const { width, height } = this.game.config
-    const centerX = width / 2
-    const centerY = height / 2
-
-    this.add
-      .text(centerX, centerY, 'Loading Game...', { color: '#000' })
-      .setOrigin(0.5)
+    this.load.start()
   }
 
   nextScene() {
-    this.loadFont('Stacked Pixel', _font, () => {
-      this.scene.start('PlayGame')
-    })
-  }
-
-  loadFont(name, url, callback) {
-    const element = document.createElement('style')
-    document.head.appendChild(element)
-    const styles = `@font-face { font-family: "${name}"; src: url("${_font}"); }\n`
-    element.sheet.insertRule(styles, 0)
-
-    WebFont.load({
-      custom: {
-        families: [name],
-      },
-      active: callback,
-    })
+    this.scene.start('PlayGame')
   }
 }
 
